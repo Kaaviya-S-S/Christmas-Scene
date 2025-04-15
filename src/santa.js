638eb21902477953b3f1caae3fd949edb811c3b9
Kaaviya S S <kaaviya.sph@gmail.com>
2025-04-15 18:46:50 +0530

    moved assets to public

--- src/santa.js
+++ src/santa.js
@@ -5,7 +5,7 @@ export function loadSanta(scene) {
     const loader = new GLTFLoader();
     
     loader.load(
-        "assets/jolly_santa.glb",  // Ensure the path to the .glb file is correct
+        "public/assets/jolly_santa.glb",  // Ensure the path to the .glb file is correct
         function (gltf) {
             // Access the loaded model (the 'scene' property contains the glTF scene)
             const santa = gltf.scene;
